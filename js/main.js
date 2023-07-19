43c9e49e63baf166a63b7f7e7ade7f84991e4637
Pedro Marins <contato@pedromarins.com>
2023-07-19 11:31:16 -0300

    Ver todos os países na página inicial

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const baseURL = "https://restcountries.com/v3.1/"
+const content = document.querySelector("#content")
 
 
 async function catchAllCountries() {
     try {
-      const response = await fetch(baseURL + "all?fields=name,flags,population,region", {
+      const response = await fetch(baseURL + "all?fields=name,flags,population,region,capital", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -17,5 +18,20 @@ async function catchAllCountries() {
   }
 
 catchAllCountries().then(function(response) {
-  console.log(response) 
+  response.forEach(country => {
+    console.log(country)
+    const card = document.createElement('figure');
+        card.innerHTML = `
+          <img class="card-image" src="${country.flags.png}" alt="">
+            <figcaption class="card-title">${country.name.common}</figcaption>
+            <ul class="card-content">
+                <li><strong>Population:</strong>${country.population}</li>
+                <li><strong>Region:</strong>${country.region}</li>
+                <li><strong>Capital:</strong>${country.capital}</li>
+            </ul>
+        </figure>
+        `;
+        card.classList.add("card")
+        content.appendChild(card);
+  });
 })
